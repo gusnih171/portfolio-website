73a42e37d551bb83a1dda0a3515bfbdfbd83c784
gusnih171 <gustav.nilsson96@gmail.com>
2024-09-24 11:19:23 +0200

    Fixed some responisve screen size errors

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -5,24 +5,25 @@ import './Logo.css';
 import PsgLogo from './photos/psg-logo.jpg';
 
 gsap.registerPlugin(TextPlugin);
-const Logo = () => {
-  
-    const textRef = useRef(null);
-  
-    useEffect(() => {
-      gsap.fromTo(textRef.current, 
-        { x: -100, opacity: 0 }, 
-      { x: 0, opacity: 1, duration: 2, text: "PSG Handball Hall Of Fame", ease:"expo.inOut", y: -15}
-      );
-    }, []);
 
-  return(
-  <div>
-    <div class="container-img">
-      <div class="psgText" ref={textRef}></div>
-      <img className="logo-psg" src={PsgLogo}/>
+const Logo = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(textRef.current, 
+      { x: -100, opacity: 0 }, 
+      { x: 0, opacity: 1, duration: 2, text: "PSG Handball Hall Of Fame", ease: "expo.inOut", y: -15 }
+    );
+  }, []);
+
+  return (
+    <div>
+      <div className="container-img">
+        <div className="psgText" ref={textRef}></div>
+        <img className="logo-psg" src={PsgLogo} alt="PSG Logo" />
+      </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default Logo;
